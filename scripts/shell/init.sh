9800e357c5090370abf997b71a6409917de9095c
Jan Philipp Bamberger <jan.philipp.bamberger@mni.thm.de>
2020-02-13 16:13:39 +0100

    cleanup

--- scripts/shell/init.sh
+++ scripts/shell/init.sh
@@ -79,7 +79,7 @@ fi
 
 echo "Datenbakrechte werden gesetzt..."
 #mysql -u "root" -p"root" -e "GRANT ALL PRIVILEGES ON '${ci_jdb_name}' . * TO '${ci_jdb_user}'@'localhost'"
-mysql -u "root" -p"root" -e "GRANT ALL PRIVILEGES ON '${ci_jdb_name}'.* TO '${ci_jdb_user}'@'localhost';"
+mysql -u "root" -p"root" -e "GRANT ALL PRIVILEGES ON *.* TO '${ci_jdb_user}'@'localhost';"
 if [ $? -eq "0" ]
 then
 	echo "OK! Datenbankrechte gesetzt."
