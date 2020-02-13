dd0fbbefba0e785ccb21af93742daaa37c4ba039
Jan Philipp Bamberger <jan.philipp.bamberger@mni.thm.de>
2020-02-13 12:19:44 +0100

    escape test #14

--- scripts/shell/init.sh
+++ scripts/shell/init.sh
@@ -70,7 +70,7 @@ fi
 
 echo "Datenbakrechte werden gesetzt..."
 #mysql -u "root" -p"root" -e "GRANT ALL PRIVILEGES ON '${ci_jdb_name}' . * TO '${ci_jdb_user}'@'localhost'"
-mysql -u "root" -p"root" -e "GRANT ALL PRIVILEGES ON '${ci_jdb_name}' TO '${ci_jdb_user}'@'localhost';"
+mysql -u "root" -p"root" -e "GRANT ALL PRIVILEGES ON '${ci_jdb_name}' TO '${ci_jdb_user}';"
 if [ $? -eq "0" ]
 then
 	echo "OK! Datenbankrechte gesetzt."
