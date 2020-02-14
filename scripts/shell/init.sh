2a2b0e332264f4455822a1d92758cb43f46d72db
Jan Philipp Bamberger <jan.philipp.bamberger@mni.thm.de>
2020-02-14 11:53:42 +0100

    added ext phase

--- scripts/shell/init.sh
+++ scripts/shell/init.sh
@@ -18,8 +18,7 @@ echo "Buildumgebung wird initialisiert..."
 
 echo "Buildvariablen werden eingelesen..."
 . build/config/joomla.build.properties.default || exit 1
-if [ -x joomla.build.properties ]
-then
+if [ -x joomla.build.properties ]; then
 	echo "Projektspezifische Buildvariablen werden eingelesen..."
 	. joomla.build.properties || exit 1
 	echo "OK! Projektspezifische Buildvariablen wurden eingelesen!"
@@ -59,8 +58,7 @@ fi
 
 echo "Datenbank wird erstellt..."
 mysql -u "root" -p"root" -e "CREATE DATABASE IF NOT EXISTS ${ci_jdb_name} CHARACTER SET utf8;"
-if [ $? -eq "0" ]
-then
+if [ $? -eq "0" ]; then
 	echo "OK! Datenbank wurde erstellt."
 else
 	echo "FAIL! Datenbank konnte nicht erstellt werden."
@@ -69,8 +67,7 @@ fi
 
 echo "Datenbanknutzer wird erstellt..."
 mysql -u "root" -p"root" -e "CREATE USER '${ci_jdb_user}'@'localhost' IDENTIFIED BY '${ci_jdb_password}';"
-if [ $? -eq "0" ]
-then
+if [ $? -eq "0" ]; then
 	echo "OK! Datenbankbenutzer erstellt."
 else
 	echo "FAIL! Datenbankbenutzer konnte nicht erstellt werden."
@@ -79,9 +76,8 @@ fi
 
 echo "Datenbakrechte werden gesetzt..."
 #mysql -u "root" -p"root" -e "GRANT ALL PRIVILEGES ON '${ci_jdb_name}' . * TO '${ci_jdb_user}'@'localhost'"
-mysql -u "root" -p"root" -e "GRANT ALL PRIVILEGES ON *.* TO '${ci_jdb_user}'@'localhost';"
-if [ $? -eq "0" ]
-then
+mysql -u "root" -p"root" -e "GRANT ALL PRIVILEGES ON *.* TO '${ci_jdb_user}'@'localhost'; FLUSH PRIVILEGES;"
+if [ $? -eq "0" ]; then
 	echo "OK! Datenbankrechte gesetzt."
 else
 	echo "FAIL! Datenbankrechte konnten nicht gesetzt werden."
@@ -94,8 +90,7 @@ echo "OK! Präfix in Datenbank ersetzt."
 
 echo "Dump wir in Datenbank eingespielt..."
 mysql -u "${ci_jdb_user}" -p"${ci_jdb_password}" "${ci_jdb_name}" < installation/sql/mysql/joomla.sql
-if [ $? -eq "0" ]
-then
+if [ $? -eq "0" ]; then
 	echo "OK! Dump in Datenbank eingespielt."
 else
 	echo "FAIL! dump konnte nicht in Datenbank eingespielt werden."
@@ -104,8 +99,7 @@ fi
 
 echo "Joomla Konfigurationsdatei wird erstellt..."
 php build/scripts/php/joomla-prepare.php --config --db_host="localhost" --db_user="${ci_jdb_user}" --db_pass="${ci_jdb_password}" --db_name="${ci_jdb_name}" --db_prefix="${ci_jdb_prefix}"
-if [ $? -eq "0" ]
-then
+if [ $? -eq "0" ]; then
 	echo "OK! Joomla Konfigurationsdatei erstellt."
 else
 	echo "FAIL! joomla Konfigurationsdatei konnte nicht erstellt werden."
@@ -123,8 +117,7 @@ echo "OK! Versionsnummer ${ci_jversion} ermittelt."
 
 echo "Schema-Tabelle wird aktualisiert..."
 mysql -u "${ci_jdb_user}" -p"${ci_jdb_password}" -e "INSERT INTO ${ci_jdb_name}.${ci_jdb_prefix}schemas (\`extension_id\`, \`version_id\`) VALUES (700, \"${ci_jversion}\")"
-if [ $? -eq "0" ]
-then
+if [ $? -eq "0" ]; then
 	echo "OK! Schema-Tabelle wurde aktualisiert."
 else
 	echo "FAIL! Schema-Tabelle konnte nicht aktualisiert werden."
@@ -135,8 +128,7 @@ echo "Passwort des Joomla Admin-Users wird verschlüsselt..."
 ci_joomla_password_crypt=$(php build/scripts/php/joomla-prepare.php --cryptpass="${ci_joomla_password}")
 echo "OK! Passwort der Joomla Admin-Users wurde verschlüsselt."
 
-if [ "${ci_jthree}" = "true"  ]
-then
+if [ "${ci_jthree}" = "true"  ]; then
 	echo "Joomla 3.x Admin Benutzer wird angelegt ..."
 	mysql -u ${ci_jdb_user} -p${ci_jdb_password} -e "INSERT INTO ${ci_jdb_name}.${ci_jdb_prefix}users (id, name, username, email, password, block, sendEmail, registerDate, lastvisitDate, activation, params, lastResetTime, resetCount) VALUES (7, \"Super User\", \"${ci_joomla_user}\", \"${ci_joomla_mail}\", \"${ci_joomla_password_crypt}\", 0, 1, \"2013-03-20 00:00:00\", \"0000-00-00 00:00:00\", 0, \"\", \"0000-00-00 00:00:00\", 0)"
 else
@@ -152,8 +144,7 @@ fi
 
 echo "Admin-User wird in die Usergroup-Map eintragen..."
 mysql -u ${ci_jdb_user} -p${ci_jdb_password} -e "INSERT INTO ${ci_jdb_name}.${ci_jdb_prefix}user_usergroup_map (user_id, group_id) VALUES (7, 8)"
-if [ $? -eq "0" ]
-then
+if [ $? -eq "0" ]; then
 	echo "OK! Admin-Benutzer wurde in die Usergroup-Map eingetragen."
 else
 	echo "FAIL! Admin-Benutzer konnte nicht in die Usergroup-Map eingetragen werden."
@@ -167,14 +158,79 @@ echo "Name der Datenbank wird ermittelt ..."
 ci_jdbconfig_db=$(php build/scripts/php/db-dump.php --jdbname)
 echo "OK! Datenbankname ${ci_jdbconfig_db} ermittelt."
 mysqldump -u "root" -p"root" ${ci_jdbconfig_db} > build/${ci_sqldump}
-if [ $? -eq "0" ]
-then
-	echo "OK! Datenbankdump ${ci_sqldump_a} wurde erfolgreich erstellt."
+if [ $? -eq "0" ]; then
+	echo "OK! Datenbankdump ${ci_sqldump} wurde erfolgreich erstellt."
 else
 	echo "FAIL! Datenbankdump konnte nicht erstellt werden."
 	exit 11
 fi
 
+echo "Datenbank ${ci_jdb_name} wird gelöscht..."
+mysql -u "root" -p"root" -e "DROP DATABASE '${ci_jdb_name}';"
+if [ $? -eq "0" ]; then
+	echo "OK! Datenbank ${ci_jdb_name} wurde erfolgreich gelöscht."
+else
+	echo "FAIL! Datenbank ${ci_jdb_name} konnte nicht gelöscht werden."
+	exit 12
+fi
+
+echo "Datenbanknutzer ${ci_jdb_user} wird entfernt..."
+mysql -u "root" -p"root" -e "DELETE FROM mysql.user WHERE User='${ci_jdb_user}'; FLUSH PRIVILEGES;"
+if [ $? -eq "0" ]; then
+	echo "OK! Benutzer ${ci_jdb_user} wurde erfolgreich entfernt."
+else
+	echo "FAIL! Benutzer ${ci_jdb_user} konnte nicht entfernt werden."
+	exit 13
+fi
+
 echo "OK! Buildumgebung wurde initialisiert."
+
+echo "Neue Datenbank wird wiederhergestellt..."
+php build/scripts/php/db-create.php --dbhost='localhost' --dbport='3306' --dbuser='root' --dbpass='root' --dump=${ci_sqldump} --jdbpass="${ci_jdb_password}" --jdbprefix="${ci_jdb_prefix}" || exit 14
+echo "OK! Datenbank wurde wiederhergestellt."
+
+echo "Sortiere alle Erweiterungen..."
+php build/scripts/php/ext-sort.php --extxmlfile=${ci_file_ext_install} || exit 15
+echo "OK! Erweiterungen wurden sortiert."
+
+echo "Beginn der Installation aller Erweiterungen..."
+php build/scripts/php/ext-install.php --user "${ci_joomla_user}" --password "${ci_joomla_password}"
+if [ -e "build/temp/extinstalltempfile" ]; then
+	echo "FAIL! Eine der Erweiterungen konnte nicht installiert werden."
+	exit 16
+else
+	echo "OK! Alle Erweiterungen wurden installiert."
+fi
+
+echo "Beginn der Deinstallation aller Erweiterungen..."
+php build/scripts/php/ext-uninstall.php --user "${ci_joomla_user}" --password "${ci_joomla_password}"
+if [ -e "build/temp/extuninstalltempfile" ]; then
+	echo "FAIL! Eine der Erweiterungen konnte nicht deinstalliert werden."
+	exit 17
+else
+	echo "OK! Alle Erweiterungen wurden deinstalliert."
+fi
+
+echo "Beginn der erneuten Installation aller Erweiterungen..."
+php build/scripts/php/ext-install.php --user "${ci_joomla_user}" --password "${ci_joomla_password}"
+if [ -e "build/temp/extinstalltempfile" ]; then
+	echo "FAIL! Eine der Erweiterungen konnte nicht erneut installiert werden."
+	exit 18
+else
+	echo "OK! Alle Erweiterungen wurden erneut installiert."
+fi
+
+echo "Datenbankdump wird erstellt..."
+echo "Name der Datenbank wird ermittelt ..."
+ci_jdbconfig_db=$(php build/scripts/php/db-dump.php --jdbname)
+echo "OK! Datenbankname ${ci_jdbconfig_db} ermittelt."
+mysqldump ${ci_jdbconfig_db} > ${ci_sqldump}
+if [ $? -eq "0" ]
+then
+	echo "OK! Datenbankdump ${ci_sqldump} wurde erfolgreich erstellt."
+else
+	echo "FAIL! Datenbankdump konnte nicht erstellt werden."
+	exit 19
+fi
 
 exit 0
