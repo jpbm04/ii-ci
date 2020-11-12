012ef8d7184f7be0f96fa8c32a9576e88daea211
Jan Philipp Bamberger <jan.philipp.bamberger@mni.thm.de>
2020-11-12 18:12:52 +0100

    test

--- scripts/shell/init.sh
+++ scripts/shell/init.sh
@@ -56,7 +56,7 @@ ci_jdb_user=$(php build/scripts/php/joomla-prepare.php --dbuser)
 echo "OK! Datenbankbenutzer ${ci_jdb_user} bezogen."
 
 echo "Datenbankmodus wird gesetzt..."
-mysql $mysqlopts -e "SET GLOBAL sql_mode = 'NO_ENGINE_SUBSTITUTION';"
+mysql -u "root" -p"root" -h "localhost" -p 3306 -e "SET GLOBAL sql_mode = 'NO_ENGINE_SUBSTITUTION';"
 if [ $? -eq "0" ]; then
 	echo "OK! Datenbankmodus wurde gesetzt."
 else
