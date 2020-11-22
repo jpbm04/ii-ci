2cbb5853203e23194a80021c34bb2a12ad8b003a
Jan Philipp Bamberger <jan.philipp.bamberger@mni.thm.de>
2020-11-22 13:23:54 +0100

    adding output while compressing

--- scripts/shell/mkzips.sh
+++ scripts/shell/mkzips.sh
@@ -5,6 +5,7 @@ mkdir zips
 pwdbak=$(pwd)
 
 ls -1 extensions | while read extension; do
+	echo "Create \"${extension}\" zip..."
 	cd extensions/$extension/$extension
 	zip -r ../../../zips/${extension} *
 	cd ${pwdbak}
