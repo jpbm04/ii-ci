97b99f38d07cea2ecd3d1f888cc893ca4e196e0d
Jan Philipp Bamberger <jan.philipp.bamberger@mni.thm.de>
2020-12-06 16:07:52 +0100

    corrected version insertion

--- scripts/shell/release.sh
+++ scripts/shell/release.sh
@@ -16,7 +16,7 @@ cd ${pwdbak}
 echo "${REPO_NAME} release succesfully created."
 
 echo "Updating update XML for \"${REPO_NAME}\"..."
-echo -ne "<updates>\n\t<update>\n\t\t<name>THM Organizer</name>\n\t\t<element>${REPO_NAME}</element>\n\t\t<type>component</type>\n\t\t<version>${GITHUB_REF:1}</version>\n\t\t<client>administrator</client>\n\t\t<infourl title=\"MNI Homepage\">https://www.thm.de/mni/</infourl>\n\t\t<targetplatform name=\"joomla\" version=\"3.*\"/>\n\t\t<maintainer>THM iCampus</maintainer>\n\t\t<maintainerurl>https://www.thm.de/</maintainerurl>\n\t\t<tags>\n\t\t\t<tag>staging</tag>\n\t\t</tags>\n\t\t<downloads>\n\t\t\t<downloadurl type=\"full\" format=\"zip\">\n\t\t\t\thttps://github.com/${REPO_USER}/${REPO_NAME}/releases/download/${GITHUB_REF}/${REPO_NAME}.zip\n\t\t\t</downloadurl>\n\t\t</downloads>\n\t</update>\n</updates>" > pages/${REPO_NAME}/updates.xml
+echo -ne "<updates>\n\t<update>\n\t\t<name>THM Organizer</name>\n\t\t<element>${REPO_NAME}</element>\n\t\t<type>component</type>\n\t\t<version>${GITHUB_REF:11}</version>\n\t\t<client>administrator</client>\n\t\t<infourl title=\"MNI Homepage\">https://www.thm.de/mni/</infourl>\n\t\t<targetplatform name=\"joomla\" version=\"3.*\"/>\n\t\t<maintainer>THM iCampus</maintainer>\n\t\t<maintainerurl>https://www.thm.de/</maintainerurl>\n\t\t<tags>\n\t\t\t<tag>staging</tag>\n\t\t</tags>\n\t\t<downloads>\n\t\t\t<downloadurl type=\"full\" format=\"zip\">\n\t\t\t\thttps://github.com/${REPO_USER}/${REPO_NAME}/releases/download/${GITHUB_REF:10}/${REPO_NAME}.zip\n\t\t\t</downloadurl>\n\t\t</downloads>\n\t</update>\n</updates>" > pages/${REPO_NAME}/updates.xml
 echo "Update XML updated."
 
 
